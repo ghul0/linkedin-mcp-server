39d8018eec43b963c30a9e072da708e0d9214ef6
Tomasz Młynek <mlynek.tomek@gmail.com>
2026-07-20 15:33:35 +0200

    fix(messaging): bind thread sends to recipient

--- tests/test_scraping.py
+++ tests/test_scraping.py
@@ -8468,6 +8468,7 @@ class TestSendMessage:
         navigate.assert_not_awaited()
 
     async def test_thread_send_rejects_wrong_visible_recipient(self, mock_page):
+        mock_page.url = "https://www.linkedin.com/messaging/thread/thread-1/"
         extractor = LinkedInExtractor(mock_page)
         send_surface = AsyncMock()
         with (
@@ -8508,6 +8509,7 @@ class TestSendMessage:
     async def test_thread_send_delegates_only_after_recipient_verification(
         self, mock_page
     ):
+        mock_page.url = "https://www.linkedin.com/messaging/thread/thread-1/"
         extractor = LinkedInExtractor(mock_page)
         expected_result = {
             "url": "https://www.linkedin.com/messaging/thread/thread-1/",
